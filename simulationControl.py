66ec2f747286bf754d33c2a6d8802109eda3f3bd
Chirag Garg <102269022+cgarg3@users.noreply.github.com>
2023-11-16 04:19:46 -0500

    Update simulationControl.py

--- simulationControl.py
+++ simulationControl.py
@@ -1,14 +1,30 @@
-class DecisionMaker:
-    def __init__(self, rule_based=True):
-        self.rule_based = rule_based
+class SimulationController:
+    def __init__(self, vehicle, path_planner, object_detector, decision_maker):
+        self.vehicle = vehicle
+        self.path_planner = path_planner
+        self.object_detector = object_detector
+        self.decision_maker = decision_maker
 
-    def make_decision(self, detected_objects, current_state):
-        if self.rule_based:
-            # Implement rule-based decision-making logic
-            # Consider traffic rules, pedestrian crossings, etc.
-            pass
-        else:
-            # Implement machine learning-based decision-making logic
-            # Use a trained model to determine the optimal action
-            pass
+    def simulate(self, start, destination):
+        # Plan path
+        waypoints = self.path_planner.plan_path(start, destination)
 
+        for waypoint in waypoints:
+            # Get sensor data
+            sensor_data = self.vehicle.get_sensor_data()
+
+            # Process sensor data for object detection
+            detected_objects = self.object_detector.process_sensor_data(sensor_data)
+
+            # Make decisions based on detected objects and current state
+            decision = self.decision_maker.make_decision(detected_objects, self.vehicle.get_state())
+
+            # Update vehicle state based on decision
+            self.vehicle.update_state(decision)
+
+            # Implement visualization for real-time observation
+
+            # Evaluate metrics (if needed)
+
+        # Simulation completed
+        print("Simulation completed.")
