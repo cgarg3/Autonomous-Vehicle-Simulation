d2b8b433dfbc84952d54fe4ccbe79a134d5e1f9c
Chirag Garg <102269022+cgarg3@users.noreply.github.com>
2023-11-16 04:43:42 -0500

    Update objectDetection.py

--- objectDetection.py
+++ objectDetection.py
@@ -1,9 +1,3 @@
-# Use a pre trained model to process the sensor data from various sensors like cameras, lidar, radar etc
-# Use of a simple object detector with pre trained model using cv2 library
-# Install the libraries:
-#   i) pip install opencv-python
-#   ii) pip install numpy
-
 import cv2
 import numpy as np
 
@@ -11,7 +5,7 @@ class ObjectDetector:
     def __init__(self, sensors):
         self.sensors = sensors
 
-        def process_sensor_data(self):
+    def process_sensor_data(self):
         # Placeholder for sensor data retrieval
         sensor_data = self.retrieve_sensor_data()
 
@@ -19,7 +13,6 @@ class ObjectDetector:
         detected_objects = self.detect_objects(sensor_data)
 
         return detected_objects
-
 
     # simulating the retrieval of sensor data
     def retrieve_sensor_data(self):
@@ -29,7 +22,7 @@ class ObjectDetector:
         dummy_image = np.zeros((480, 640, 3), dtype=np.uint8)
         return dummy_image
 
-    # simulating object detection using a pre trained model
+    # simulating object detection using a pre-trained model
     def detect_objects(self, sensor_data):
         # Placeholder for object detection using a pre-trained model
         # In a real implementation, you would use a suitable object detection model
@@ -41,12 +34,53 @@ class ObjectDetector:
         ]
         return dummy_detection_result
 
+class VehicleSimulation:
+    def __init__(self, map_data, sensors):
+        self.path_planner = PathPlanner(map_data)
+        self.object_detector = ObjectDetector(sensors)
+        self.current_pose = (0, 0, 0)  # Initial pose of the vehicle
+        self.obstacles = []  # List to store obstacle positions
+        self.path = []  # List to store the planned path
+
+    def update_obstacles(self):
+        # Update obstacle positions using the object detector
+        self.obstacles = [obj['position'] for obj in self.object_detector.process_sensor_data()]
+
+    def simulate(self, destination):
+        # Plan the global path
+        self.path = self.path_planner.plan_path(self.current_pose, destination)
+
+        while self.path:
+            # Update obstacle positions
+            self.update_obstacles()
+
+            # Get the current waypoint
+            current_waypoint = self.path[0]
+
+            # Local planning to adjust the path based on obstacles
+            local_adjustment = self.path_planner.local_plan(self.current_pose, self.obstacles)
+
+            # Update the current pose based on the local adjustment
+            self.current_pose = local_adjustment[-1] if local_adjustment else current_waypoint
+
+            # Remove the first waypoint from the path
+            self.path = self.path[1:]
+
+            # Simulate the vehicle movement
+            print(f"Vehicle is now at position: {self.current_pose}")
+
 # Example usage:
+map_data = [
+    [0, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+]
 sensors = ["camera", "lidar", "radar"]
-object_detector = ObjectDetector(sensors)
-detected_objects = object_detector.process_sensor_data()
+vehicle_simulation = VehicleSimulation(map_data, sensors)
 
-print("Detected Objects:")
-for obj in detected_objects:
-    print(f"Class: {obj['class']}, Position: {obj['position']}")
+# Define the destination
+destination_point = (3, 3, 0)
 
+# Simulate the vehicle movement
+vehicle_simulation.simulate(destination_point)
