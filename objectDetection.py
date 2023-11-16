e72b3d49ddf9cbab2d3bfd5d68a3a07951e28d97
Chirag Garg <102269022+cgarg3@users.noreply.github.com>
2023-11-16 04:18:25 -0500

    Create objectDetection.py

---
+++ objectDetection.py
@@ -0,0 +1,9 @@
+class ObjectDetector:
+    def __init__(self, sensors):
+        self.sensors = sensors
+
+    def process_sensor_data(self):
+        # Retrieve sensor data from cameras, lidar, radar, etc.
+        # Implement object detection using a pre-trained model (e.g., YOLO or SSD)
+        # Return a list of detected objects with their positions and classifications
+        pass
