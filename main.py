f46752b6def6ada106dfc068327776037194901c
Chirag Garg <102269022+cgarg3@users.noreply.github.com>
2023-11-16 05:21:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ from vehicle_simulation import VehicleSimulation
 from object_detector import ObjectDetector
 from decision_maker import DecisionMaker
 
+# Main script
+if __name__ == "__main__":
+    # Connect to Carla server
+    client = carla.Client('localhost', 2000)
+    client.set_timeout(2.0)
+
+    # Load Carla world
+    world = client.get_world()
+
+    # Define your map_data and sensors
+    map_data = ...  # Define your map data
+    sensors = [...]  # Define your sensors
+
+    # Initialize your simulation
+    vehicle_simulation = VehicleSimulation(map_data, sensors, world)
+
+    # Start the simulation
+    destination = ...  # Define your destination
+    vehicle_simulation.simulate(destination)
