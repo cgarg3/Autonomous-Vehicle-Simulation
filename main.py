5e2fbd58303fc7f06b8e77e9b918f279f6fb331a
Chirag Garg <102269022+cgarg3@users.noreply.github.com>
2023-11-16 05:18:49 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# main.py
+from path_planner import PathPlanner
+from simulation_controller import SimulationController
+from vehicle_simulation import VehicleSimulation
+from object_detector import ObjectDetector
+from decision_maker import DecisionMaker
+
