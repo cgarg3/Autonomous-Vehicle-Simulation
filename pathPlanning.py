9e9be737060fd624d6cccbf3e198f60e43e13c47
Chirag Garg <102269022+cgarg3@users.noreply.github.com>
2023-11-16 04:18:00 -0500

    Update pathPlanning.py

--- pathPlanning.py
+++ pathPlanning.py
@@ -1,1 +1,14 @@
+class PathPlanner:
+    def __init__(self, map_data):
+        self.map_data = map_data
+
+    def plan_path(self, start, destination):
+        # Implement pathfinding algorithm (e.g., A* or Dijkstra's)
+        # Return a list of waypoints representing the planned path
+        pass
+
+    def local_plan(self, current_pose, obstacles):
+        # Implement local planning algorithm to adjust the path based on dynamic obstacles
+        # Return updated waypoints
+        pass
 
