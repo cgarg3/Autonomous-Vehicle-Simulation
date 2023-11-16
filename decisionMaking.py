344eb0827803e16a0742e2624096c75529cfef95
Chirag Garg <102269022+cgarg3@users.noreply.github.com>
2023-11-16 04:20:19 -0500

    Update decisionMaking.py

--- decisionMaking.py
+++ decisionMaking.py
@@ -1,1 +1,14 @@
+class DecisionMaker:
+    def __init__(self, rule_based=True):
+        self.rule_based = rule_based
+
+    def make_decision(self, detected_objects, current_state):
+        if self.rule_based:
+            # Implement rule-based decision-making logic
+            # Consider traffic rules, pedestrian crossings, etc.
+            pass
+        else:
+            # Implement machine learning-based decision-making logic
+            # Use a trained model to determine the optimal action
+            pass
 
